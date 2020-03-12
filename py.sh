981ca52393a32f12e9379b08a63e7bb10f46e626
ikarino <ggrks218@gmail.com>
2020-03-12 14:29:15 +0900

    modification in py.sh

--- py.sh
+++ py.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
+# compile script for python library
+
 # windows
-nim c --threads:on --app:lib --out:python/pyDeepTown.pyd src/pyDeepTown.nim
+nim c -d:release --threads:on --app:lib --out:python/pyDeepTown.pyd src/pyDeepTown.nim
 # linux
-# nim c -d:release --tlsEmulation:off --app:lib --out:python/pyDeepTown.so src/pyDeepTown.nim
+# nim c -d:release --threads:on --app:lib --out:python/pyDeepTown.so src/pyDeepTown.nim
